0cfcedfec66d6531a3ee1c179b455b30e44dd4aa
Youngung Yun <yun990325tt@gmail.com>
2025-11-05 03:09:30 +0900

    auto release

--- go/excellent/main.go
+++ go/excellent/main.go
@@ -1,9 +1,9 @@
 package main
 
-func EvenOrOdd(number int) string {
-	if number % 2 == 0 {
-		return "even"
-	} else {
-		return "odd"
-	}
+import "fmt"
+
+var version string
+
+func main() {
+	fmt.Printf("Example %s\n", version)
 }
